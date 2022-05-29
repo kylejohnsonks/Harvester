57a2ab7245e1dc41ac64f6311a23d2464bcdd536
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2022-05-29 12:52:08 -0500

    seedling dropdowns

--- iharvester/app.py
+++ iharvester/app.py
@@ -93,13 +93,13 @@ def dropdowns():
             sl_ids.append(row[0])
 
     # #Update seedling, seedling IDs
-    # stmt=select(s_meta.c.id).where(s_meta.c.germinated='').order_by(s_meta.c.id)
+    # stmt=select(s_meta.c.id).where(s_meta.c.germinated=='').order_by(s_meta.c.id)
     # with Session(engine) as session:
     #     for row in session.execute(stmt):
     #         s_update_ids.append(row[0])
 
     # #New plant, seedling ID
-    # stmt=select(s_meta.c.id).where(s_meta.c.germinated='True').order_by(s_meta.c.id)
+    stmt=select(s_meta.c.id).where(s_meta.c.germinated=='true').order_by(s_meta.c.id)
     stmt=select(s_meta.c.id).order_by(s_meta.c.id)
     with Session(engine) as session:
         for row in session.execute(stmt):
